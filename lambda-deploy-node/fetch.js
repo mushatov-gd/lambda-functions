986d12d15da9f87ac4fe97a9dcd10f443b055b35
mushatov-gd <mushatov@griddynamics.com>
2022-09-09 11:35:44 +0200

    2 files in lambda

---
+++ lambda-deploy-node/fetch.js
@@ -0,0 +1,52 @@
+const https = require('https');
+
+module.exports = async function fetch(method, url, body) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      method,
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    };
+  
+    const req = https.request(url, options, (res) => {
+      let result = '';
+
+      res.on('end', () => {
+        let body;
+        try {
+          body = JSON.parse(result);
+        } catch (e) {
+          resolve({
+            url,
+            body,
+            status: result,
+          });
+          
+          return;
+        }
+        resolve({
+          url,
+          body,
+          status: res.statusCode,
+        });
+      });
+
+      res.on('data', (d) => {
+        if (res.statusCode >= 300) {
+          reject({
+            url,
+            status: res.statusCode,
+            error: d.toString(),
+          });
+        } else {
+          result += d.toString();
+        }
+      });
+    });
+    if (body) {
+        req.write(JSON.stringify(body));
+    }
+    req.end();
+  });
+}
--- lambda-deploy-node/index.js
+++ lambda-deploy-node/index.js
@@ -1,8 +1,58 @@
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('test multiple dirs'),
-    };
-    return response;
+const fetch = require('./fetch.js');
+
+const APIStrategy = {
+    stage: 'https://test.stage.grib.net',
+    production: 'https://www.grib.com',
 };
+const WebsiteStrategy = {
+    stage: 'https://ir.stage.grib.net', // replace with ir stage env with prod deployment if it ever becomes a reality
+    production: 'https://ir.grib.com',
+};
+const pressReleasesClearCacheEndpoint = '/api/financial/press-releases?password=blabla';
+const butterClearCacheEndpoint = '/api/butter/cache';
+const websiteClearCacheEndpoint = '/document-cache';
+const api = APIStrategy[process.env.ENVIRONMENT ?? 'stage'] ?? 'https://test.stage.grib.net';
+const website = WebsiteStrategy[process.env.ENVIRONMENT ?? 'stage'] ?? 'https://ir.stage.grib.net';
+
+exports.handler = async (event, context) => {
+    let body = null;
+    try {
+        body = JSON.parse(event?.body);
+    } catch (e) {
+        console.error(JSON.stringify({ type: 'error', reason: 'ir-clear-cache-lambda', description: 'Failed to parse event body', message: e.toString() }));
+    }
+    console.log(JSON.stringify({ type: 'ir-notifier', reason: 'ir-clear-cache-lambda', message: `Process API Gateway request for "${JSON.stringify(body)}"` }));
+    
+    const pageType = body?.data?.page_type;
+    let e;
+    if (pageType === 'investors_press_release') {
+        e = await clearCache(true);
+    } else if (pageType === 'investors_events') {
+        e = await clearCache();
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(e),
+    };
+};
+
+async function clearCache(clearPressReleases) {
+    try {
+        if (clearPressReleases) {
+            await fetch('DELETE', `${api}${pressReleasesClearCacheEndpoint}`);
+        }
+        await fetch('DELETE', `${api}${butterClearCacheEndpoint}`);
+        await fetch('DELETE', `${website}${websiteClearCacheEndpoint}`);
+    } catch (e) {
+        console.error(JSON.stringify({ type: 'error', reason: 'ir-clear-cache-lambda',  message: JSON.stringify(e) }));
+        
+        return e;
+    }
+    console.log(JSON.stringify({ 
+        type: 'ir-notifier',
+        reason: 'ir-clear-cache-lambda',
+        message: `Successfully cleared cache for ${clearPressReleases ? 'investor press-releases' : 'investor events'}`
+    }));
+    return { ok: 'true' };
+}
